89a083815df0c434ff75fc86ac93c4d771f4481b
Forrest Reiland <you@example.com>
2020-09-04 14:32:34 -0700

    add second test suite to test old age function, create userinfo object, test pass

--- __tests__/bl.test.js
+++ __tests__/bl.test.js
@@ -58,8 +58,3 @@ describe('keyInfo class', () => {
 });
 
 
-
-
-
-
-
---
+++ __tests__/oldage.test.js
@@ -0,0 +1,17 @@
+import {keyInfo, raceExpect, timeOnEarth} from '../src/js/blscripts.js';
+
+
+describe('old age test', () => {
+
+  let userInfo;
+
+  beforeEach(() => {
+    userInfo = new keyInfo(90,'African American');
+  });
+
+  test('should return a user info object with 90 as age and african american as race', () => {
+    expect(userInfo.age).toEqual(90);
+    expect(userInfo.race).toEqual('African American');
+  });
+
+});
