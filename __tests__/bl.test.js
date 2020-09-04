218ebb41576ecac883e4d45ecd4212f19ffaaccd
Forrest Reiland <you@example.com>
2020-09-04 10:09:41 -0700

    test fail no code added in

--- __tests__/bl.test.js
+++ __tests__/bl.test.js
@@ -3,6 +3,7 @@ import {keyInfo} from '../src/js/blscripts.js';
 
 describe ('keyInfo class with input user personal data', () => {
   test('should correctly create a keyInfo class with a users age and race')
-  
+      const newUser = new keyInfo (25, hispanic);
+      
 
 })
